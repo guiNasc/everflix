8831a2ce17fb0b4191d81295d151ce3f75a7c614
Guilherme Nascimento <gui.heavy@gmail.com>
2020-07-29 22:04:28 -0300

    Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Logo from '../../assets/img/everflix.png'
 import './Menu.css';
 import Button from '../Button';
+import {Link} from 'react-router-dom';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
             <img className="Logo" src={Logo} alt="Everflix logo"/>
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
